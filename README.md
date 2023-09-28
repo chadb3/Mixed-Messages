7a0c8e8d0f45ab0f8ea2b42416a06ddc14897bf1
Chad <chadb78999@gmail.com>
2023-09-28 07:31:05 -0500

    added random number function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mixed-Messages
 Mixed Messages: Message generator program. Will Output Random Messages to users.
+
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const MM = "Mixed Messages";
 const welcome = `Welcome to ${MM}`;
 console.log(welcome);
+// Standard function to generate Random Number
+// Default Parameter is 10 such that getRandom() === getRandom(10)
+// Default parameter may change and is a place holder at this point
+// Math.floor(Math.random()*(MaxNumber); // is the standard way to generate a random number. MaxNumber is the max value.
+const getRandom=(maxNum=10)=>
+{
+ return Math.floor(Math.random()*maxNum);
+}
+
+// Test Functions
+// Generate Random Number (Default parameter (10))
+console.log(getRandom());
+// Generate Random Number 2 (Default parameter (10))
+console.log(getRandom());
+// Generate Random Number 3 (input 999);
+console.log(getRandom(999));
+
